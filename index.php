ec4df144474c8f6c700c225aa36b4b48108ec77a
Persuasive Archives <mediapersuasive@gmail.com>
2025-08-06 17:26:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,2 @@
-<?php
-$boards = array_filter(glob("boards/*"), "is_dir");
-?>
-
-<!DOCTYPE html>
-<html>
-<head><title>FluxChan</title></head>
-<body>
-     <link rel="stylesheet" href="styles/dark.css">
-<h1>Boards</h1>
-<ul>
-<?php foreach ($boards as $path): 
-    $board = basename($path); ?>
-    <li><a href="board.php?board=<?= $board ?>">/<?= $board ?>/</a></li>
-<?php endforeach; ?>
-</ul>
-<footer>&copy powered by <a href="https://github.com/PersuasivFoundation/FluxChan">Fluxchan</a>
-</body>
-</html>
+// DO NOT USE THIS REPO
+// I FORGOT MY FUCKING EMAIL.
